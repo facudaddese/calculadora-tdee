c9962365bafb8399a77e445c8d800d4fdfef64b4
Facundo D'addese <facundo.daddese19@gmail.com>
2026-09-05 15:50:33 -0300

    modified:   src/components/hooks/useCalculator.ts

--- src/components/hooks/useCalculator.ts
+++ src/components/hooks/useCalculator.ts
@@ -2,7 +2,6 @@ import { useEffect, useReducer } from "react";
 import type { Data, State } from "../../types/Types";
 import { initialState, reducer } from "../../reducers/reducer";
 
-
 export const useCalculator = () => {
   const [state, dispatch] = useReducer(reducer, initialState, () => {
     const stored = localStorage.getItem("form");
